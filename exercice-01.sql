3ddb8ed499bbc0e7cd9e382299664ec3a8119a90
tonyverc <135614394+tonyverc@users.noreply.github.com>
2023-07-04 16:52:57 +0200

    Update exercice-01.sql

--- exercice-01.sql
+++ exercice-01.sql
@@ -8,24 +8,43 @@ FROM tag;
 
 -- Exo 1.1
 -- Écrivez la requête qui permet de lister tous les students
+SELECT *
+FROM student;
 
 -- Exo 1.2
 -- Écrivez la requête qui permet de lister tous les projects
+SELECT *
+FROM project;
 
 -- Exo 1.3
 -- Écrivez la requête qui permet de lister le student dont l'id est `2`
+SELECT * 
+FROM student 
+WHERE id = 2;
 
 -- Exo 1.4
 -- Écrivez la requête qui permet de lister les students dont l'id n'est pas `2`
+SELECT *
+FROM student
+WHERE id <> 2;
 
 -- Exo 1.5
 -- Écrivez la requête qui permet de lister le project dont l'id est `3`
+SELECT *
+FROM project
+WHERE id = 3;
 
 -- Exo 1.6
 -- Écrivez la requête qui permet de lister les projects dont l'id n'est pas `3`
+SELECT *
+FROM project
+WHERE id <> 3;
 
 -- Exo 1.7
 -- Écrivez la requête qui permet de lister les students dont l'email se termine par la chaîne de caractères `.com`
+SELECT *
+FROM student
+WHERE email LIKE '%.com';
 
 -- Exo 1.8
 -- Écrivez la requête qui permet de lister les students dont la date de création est antérieure au 10 janvier 2021 inclus
