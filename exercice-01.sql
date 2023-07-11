efb6c5507dcd38ebb71534fbd50d66c064e8b244
tonyverc <135614394+tonyverc@users.noreply.github.com>
2023-07-11 16:07:54 +0200

    Update exercice-01.sql

--- exercice-01.sql
+++ exercice-01.sql
@@ -48,18 +48,34 @@ WHERE email LIKE '%.com';
 
 -- Exo 1.8
 -- Écrivez la requête qui permet de lister les students dont la date de création est antérieure au 10 janvier 2021 inclus
+SELECT *
+FROM student
+WHERE (created_at <= '2021-01-10 00:00:00');
 
 -- Exo 1.9
 -- Écrivez la requête qui permet de lister les projects dont la description ne contient pas la chaîne de caractères `Dolores`
+SELECT description
+FROM project
+WHERE description NOT LIKE 'Dolores';
 
 -- Exo 1.10
 -- Écrivez la requête qui permet de lister les projects dont la date de création est postérieure au 1er juillet 2021 inclus
+SELECT start_date
+FROM project
+WHERE (start_date >= '2021-07-01 00:00:00');
 
 -- Exo 1.11
 -- Écrivez la requête qui permet de lister les students ayant un project
 -- Note : les students sans project ont une colonne `project_id` qui est nulle
+SELECT *
+FROM student
+WHERE project_id IS NOT NULL;
+
 
 -- Exo 1.12
 -- Écrivez la requête qui permet de lister les students n'ayant pas de project
 -- Note : les students sans project ont une colonne `project_id` qui est nulle
+SELECT *
+FROM student
+WHERE project_id IS NULL;
 
